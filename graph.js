af3457488ac47011216eef7e8703899ce7f11217
viva79 <78827959+viva79@users.noreply.github.com>
2025-12-16 12:41:51 -0600

    Add files via upload

---
+++ graph.js
@@ -0,0 +1,70 @@
+const svg = d3.select("svg");
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+d3.json("equinox-experience.json").then(data => {
+
+  const simulation = d3.forceSimulation(data.nodes)
+    .force("link", d3.forceLink(data.links).id(d => d.id).distance(120))
+    .force("charge", d3.forceManyBody().strength(-300))
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
+  const link = svg.append("g")
+    .selectAll("line")
+    .data(data.links)
+    .enter()
+    .append("line")
+    .attr("class", "link");
+
+  const node = svg.append("g")
+    .selectAll(".node")
+    .data(data.nodes)
+    .enter()
+    .append("g")
+    .attr("class", "node")
+    .call(d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended)
+    );
+
+  node.append("circle")
+    .attr("r", 18);
+
+  node.append("text")
+    .attr("x", 22)
+    .attr("y", 4)
+    .text(d => d.id);
+
+  node.append("title")
+    .text(d => d.description);
+
+  simulation.on("tick", () => {
+    link
+      .attr("x1", d => d.source.x)
+      .attr("y1", d => d.source.y)
+      .attr("x2", d => d.target.x)
+      .attr("y2", d => d.target.y);
+
+    node
+      .attr("transform", d => `translate(${d.x}, ${d.y})`);
+  });
+
+  function dragstarted(event, d) {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+
+  function dragged(event, d) {
+    d.fx = event.x;
+    d.fy = event.y;
+  }
+
+  function dragended(event, d) {
+    if (!event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
+
+});
